b499fe940fe77fbf52597cf28eb5d9f1b164a207
mittaltanmay <139030702+mittaltanmay@users.noreply.github.com>
2026-09-02 15:33:31 +0530

    Step 8: apply_standardizer

--- model.py
+++ model.py
@@ -99,8 +99,10 @@ def fit_standardizer(X):
             std[i]=1.0
     return (mean,std)
 
-# Step 8 - apply_standardizer (not yet solved)
-# TODO: implement
+# Step 8 - apply_standardizer
+def apply_standardizer(X, mean, std):
+    # TODO: Return the scaled matrix (X - mean) / std via broadcasting.
+    return (X-mean)/std
 
 # Step 9 - add_bias_column (not yet solved)
 # TODO: implement
